7193b5eafee55513de8cc5594b955927344d684d
22670060-ops <22670060@itiguala.edu.mx>
2025-09-20 23:37:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, redirect
 import datetime
+import os
 
 app = Flask(__name__)
+
+# Redirigir la raíz a /status
+@app.route('/')
+def home():
+    return redirect('/status')
 
 @app.route('/status')
 def status():
     return jsonify({
-        "service":"demo-cloud", 
-        "version":"1.0",
-        "timestamp":datetime.datetime.utcnow().isoformat()+"Z"
+        "service": "demo-cloud", 
+        "version": "1.0",
+        "timestamp": datetime.datetime.utcnow().isoformat() + "Z"
     })
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
